a8dbec08ac9bd23bd8a72f2e86f43848001b6cb4
Skuskusku13 <danlevy149@gmail.com>
2022-11-30 22:07:23 +0100

    responsive refonte index + darmode

--- js/script.js
+++ js/script.js
@@ -1,84 +1,114 @@
-
-// menu burger 
+// menu burger
 
 let openBurger = document.querySelector(".open");
 let closeBurger = document.querySelector(".close");
 let menu = document.querySelector(".menu");
 
+openBurger.addEventListener("click", () => {
+  menu.classList.add("active");
+  openBurger.classList.add("none");
+  closeBurger.classList.add("block");
+});
 
-openBurger.addEventListener('click', () => {
-    menu.classList.add('active')
-    openBurger.classList.add('none');
-    closeBurger.classList.add('block');
-    
-})
+closeBurger.addEventListener("click", () => {
+  menu.classList.remove("active");
+  closeBurger.classList.remove("block");
+  openBurger.classList.remove("none");
+});
 
-closeBurger.addEventListener('click', () => {
-    menu.classList.remove('active');
-    closeBurger.classList.remove('block');
-    openBurger.classList.remove('none');
-    
-})
+// changement de couleur en dark
 
+let body = document.querySelector("body");
+let colors = document.querySelector(".colors");
 
+let rememberColors = document.querySelector(".colors").textContent;
 
+function dark() {
+  if (rememberColors == "DARK") {
+    colors.innerHTML = "LIGHT";
+    colors.style.backgroundColor = "#F1F1F1";
+    colors.style.color = "black";
+  }
+}
+
+// rememberColors.addEventListener('click', () => {
+//     rem = rememberColors
+// })
+
+// if(rem == "LIGHT") {
+//     colors.innerHTML="LIGHT";
+//     colors.style.backgroundColor="#F1F1F1";
+//     colors.style.color="black";
+// }
+
+colors.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (colors.textContent == "DARK") {
+    colors.innerHTML = "LIGHT";
+    colors.style.backgroundColor = "#F1F1F1";
+    colors.style.color = "black";
+  } else {
+    colors.innerHTML = "DARK";
+    colors.style.backgroundColor = "#000000cf";
+    colors.style.color = "aliceblue";
+  }
+});
+
+// console.log(rem);
 
 let list = document.querySelectorAll(".list-title");
 // console.log(list)
 
-
-list.forEach(elt => {
-    elt.addEventListener('click', () => {
-        elt.classList.toggle("active");
-    })
+list.forEach((elt) => {
+  elt.addEventListener("click", () => {
+    elt.classList.toggle("active");
+  });
 });
 
+// traitement du formulaire de contact
 
-
-// traitement du formulaire de contact 
-
-let Error = document.getElementById('Error');
+let Error = document.getElementById("Error");
 
 function traiterEmail() {
-    let email = document.getElementById('email').value;
+  let email = document.getElementById("email").value;
 
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-        document.getElementById("email").style.backgroundColor = "#71bf71";
-    } else {
-        document.getElementById("email").style.backgroundColor = "#e24d36";
-        Error.innerHTML="Email invalide"
-    }
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    document.getElementById("email").style.backgroundColor = "#71bf71";
+  } else {
+    document.getElementById("email").style.backgroundColor = "#e24d36";
+    Error.innerHTML = "Email invalide";
+  }
 }
 
 function traiterNom() {
-    let nom = document.getElementById('nom').value;
+  let nom = document.getElementById("nom").value;
 
-    if (nom != "") {
-        document.getElementById("nom").classList.add('validName');
-    } else {
-        document.getElementById("nom").style.backgroundColor = "#e24d36";
-        Error.innerHTML="Veuillez mettre un nom";
-      }
+  if (nom != "") {
+    document.getElementById("nom").classList.add("validName");
+  } else {
+    document.getElementById("nom").style.backgroundColor = "#e24d36";
+    Error.innerHTML = "Veuillez mettre un nom";
+  }
 }
 
 function traiterMess() {
-    let mess = document.getElementById('mess').value;
+  let mess = document.getElementById("mess").value;
 
-    if (mess != "") {
-        document.getElementById("mess").style.backgroundColor = "#71bf71";
-    } else {
-        document.getElementById("mess").style.backgroundColor = "#e24d36";
-        Error.innerHTML="Veuillez mettre un message";
-      }
+  if (mess != "") {
+    document.getElementById("mess").style.backgroundColor = "#71bf71";
+  } else {
+    document.getElementById("mess").style.backgroundColor = "#e24d36";
+    Error.innerHTML = "Veuillez mettre un message";
+  }
 }
 
 function traiterTel() {
-    let tel = document.getElementById('tel').value;
+  let tel = document.getElementById("tel").value;
 
-    if (tel != "") {
-        document.getElementById("mess").style.backgroundColor = "#71bf71";
-    } else {
-        document.getElementById("mess").style.backgroundColor = "#e24d36";
-        Error.innerHTML="Veuillez mettre un message";
-      }
+  if (tel != "") {
+    document.getElementById("mess").style.backgroundColor = "#71bf71";
+  } else {
+    document.getElementById("mess").style.backgroundColor = "#e24d36";
+    Error.innerHTML = "Veuillez mettre un message";
+  }
 }
--- verif.php
+++ verif.php
@@ -28,9 +28,30 @@ if(isset($_POST['Valider'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="responsive/styles_g.css" />
+    <title>Message Envoyé</title>
 </head>
 <body>
+    <header>
+      <nav class="menu-principal">
+        <div class="logo-navbar">
+          <a href="index.html">
+            <img class="navbar-image" src="www/cv.png" alt="logo du site" />
+          </a>
+        </div>
+        <i class="open"></i>
+        <i class="close"></i>
+        <ul class="menu">
+          <li><a class="pages-menu" href="index.html">Home</a></li>
+          <li><a class="pages-menu" href="cv.html">CV</a></li>
+          <li><a class="pages-menu" href="formation.html">Formation</a></li>
+          <li><a class="pages-menu" href="entreprise.html">Entreprise</a></li>
+          <li><a class="pages-menu" href="projets.html">Projets</a></li>
+          <li><a class="pages-menu" href="veilles.html">Veilles</a></li>
+          <li><a class="pages-menu" href="contact.html">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
     <a href="index.html">Retourner à la page d'accueil</a>
 </body>
 </html>
